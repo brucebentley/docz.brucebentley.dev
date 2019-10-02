3d16b98ad6e9965cab3a34702a10ad62860cc883
Bruce Bentley <brucebentley@me.com>
2019-10-02 15:52:16 -0400

    Removing the mis-compiled dest/ directory

--- public/public/styles-96a9c969ed659b0ed74e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{375:function(n,w,o){}}]);
-//# sourceMappingURL=styles-96a9c969ed659b0ed74e.js.map
--- public/styles-d5324d88e4832160def7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{375:function(n,w,o){}}]);
-//# sourceMappingURL=styles-d5324d88e4832160def7.js.map
